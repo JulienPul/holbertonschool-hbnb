768ff2028ebb63b8377a76922747a1f5d05a583f
Pulon Julien <pulonjulien@gmail.com>
2025-07-22 12:42:59 +0200

    add header and footer to script.js

--- part4/base_files/scripts.js
+++ part4/base_files/scripts.js
@@ -3,6 +3,19 @@
   Please, follow the project instructions to complete the tasks.
 */
 
-document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("header").innerHTML = `
+    <nav>
+      <a href="index.html" class="logo">
+        <img src="images/logo.png" alt="MyBnB logo" />
+      </a>
+      <a href="login.html" class="login-button">Login</a>
+    </nav>
+  `;
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("footer").innerHTML = `
+    <p>&copy; 2025 MyBnB. All rights reserved.</p>
+  `;
+});
