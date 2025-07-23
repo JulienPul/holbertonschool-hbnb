05cae7d55c6b14bc2ea9dcab6eb2f4a6b75bfe64
Pulon Julien <pulonjulien@gmail.com>
2025-07-23 14:23:39 +0200

    Add an event listener to the login form to handle the form submission.

--- part4/base_files/scripts.js
+++ part4/base_files/scripts.js
@@ -120,5 +120,19 @@ document.addEventListener("DOMContentLoaded", () => {
       `;
     }
   }
+   const loginForm = document.getElementById("login-form");
+
+  if (loginForm) {
+    loginForm.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const email = document.getElementById("email").value.trim();
+      const password = document.getElementById("password").value.trim();
+
+      console.log("Email:", email);
+      console.log("Password:", password);
+
+    });
+  }
 });
 
